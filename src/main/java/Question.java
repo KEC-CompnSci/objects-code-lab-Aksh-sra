f6a71122e855a44460dd28ad57138583aa34aad3
Aksh-sra <25srana@retsdstudent.ca>
2025-04-21 16:14:49 +0000

    main.java

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -2,5 +2,36 @@ import java.util.Scanner;
 
 
 public class Question {
-  
+    //Instant Variables
+    private String questions;
+    private int totalMarks; 
+    private String answer; 
+    private String studentAnswer; 
+
+
+    //constructor 
+    public Question(String questions, int totalMarks, String answer, String studentAnswer) {
+    this.question = question; 
+    this.totalMarks = totalMarks; 
+    this.answer = answer;
+    this.studentAnswer = ""; 
 }
+
+//method to ask question
+public void AskQuestions(Scanner Scanner) {
+    Systren.out.println(question + "(" + totalMarks + "marks)"); 
+    System.out.println("YourAnswer: ");
+    studentAnswer = scanner.nextLine(); 
+}
+
+//method to check answer 
+public int CheckAnswer() {
+    if (studentAnswer != null && studentAnswer.equals(answer)) {
+        return totalMarks;
+    } else {
+        return 0;
+    }
+}
+
+}
+
